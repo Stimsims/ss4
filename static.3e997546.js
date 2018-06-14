a2ec1f1c982b917f7ba038ba5974acc7cadb97ae
illulli <illu.development@gmail.com>
2018-06-15 08:49:06 +1000

    game tree working

--- static.3e997546.js
+++ static.3e997546.js
@@ -67,7 +67,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/";
+/******/ 	__webpack_require__.p = "https://illulli-1e5a.com/";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 52);
@@ -4999,4 +4999,4 @@ exports.push([module.i, "body{font-family:HelveticaNeue-Light,Helvetica Neue Lig
 /***/ })
 /******/ ]);
 });
-//# sourceMappingURL=static.770fb2c9.js.map
+//# sourceMappingURL=static.3e997546.js.map
