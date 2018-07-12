2e84ee649a7312fb362ba23f5dd38849eb2c190a
illulli <illu.development@gmail.com>
2018-07-12 21:11:07 +1000

    pre nerdamer

--- dist/static.c7a27c31.js
+++ dist/static.c7a27c31.js
@@ -67,7 +67,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/";
+/******/ 	__webpack_require__.p = "https://illulli-1e5a.com/";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 73);
@@ -7459,4 +7459,4 @@ exports.push([module.i, "*{-webkit-box-sizing:border-box;box-sizing:border-box}b
 /***/ })
 /******/ ]);
 });
-//# sourceMappingURL=static.44cc2022.js.map
+//# sourceMappingURL=static.c7a27c31.js.map
