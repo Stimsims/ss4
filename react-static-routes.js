bc4fca9e2626ca2e45118071e2c5f3141e744cfb
illulli <illu.development@gmail.com>
2018-08-09 13:32:37 +1000

    tag pages working again

--- react-static-routes.js
+++ react-static-routes.js
@@ -19,14 +19,15 @@ const universalOptions = {
   },
 }
 
-  const t_0 = universal(import('../src/containers/Home.jsx'), universalOptions)
+  const t_0 = universal(import('../src/containers/Posts.jsx'), universalOptions)
 const t_1 = universal(import('../src/containers/Post.jsx'), universalOptions)
-const t_2 = universal(import('../src/containers/Posts.jsx'), universalOptions)
+const t_2 = universal(import('../src/containers/Home.jsx'), universalOptions)
 const t_3 = universal(import('../src/containers/Games.jsx'), universalOptions)
 const t_4 = universal(import('../src/containers/Game.jsx'), universalOptions)
 const t_5 = universal(import('../src/containers/About.jsx'), universalOptions)
 const t_6 = universal(import('../src/containers/Settings.jsx'), universalOptions)
-const t_7 = universal(import('../src/containers/404.jsx'), universalOptions)
+const t_7 = universal(import('../src/containers/Privacy.jsx'), universalOptions)
+const t_8 = universal(import('../src/containers/404.jsx'), universalOptions)
 
 
 // Template Map
@@ -38,12 +39,13 @@ t_3,
 t_4,
 t_5,
 t_6,
-t_7
+t_7,
+t_8
 ]
 
 // Template Tree
 global.templateIDsByPath = global.templateIDsByPath || {
-  '404': 7
+  '404': 8
 }
 
 // Get template for given path
