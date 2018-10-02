d28f525c113567e4860d0ee6794c4f382606d470
illulli <illu.development@gmail.com>
2018-10-02 21:34:35 +1000

    google share button not finding URL on local

--- src/containers/Post.jsx
+++ src/containers/Post.jsx
@@ -53,9 +53,9 @@ class Post extends React.Component{
         //   // Call the render method when appropriate within your app to display
         //   // the button.
         //   gapi.interactivepost.render('sharePost', options);
-          //gapi.plus.render('gplus', {"contenturl": "http://illulli-1e5a.com/posts/hello/", "href": "https://illulli-1e5a.com/posts/hello/", 
-          //"width": "200", "height": "20", "theme": "light"});
-          gapi.plus.go('gplus');
+          gapi.plus.render('gplus', {"contenturl": "http://illulli-1e5a.com/posts/hello/", "href": "https://illulli-1e5a.com/posts/hello/", 
+          "width": "200", "height": "20", "theme": "light"});
+         // gapi.plus.go('gplus');
     }
     renderText(){
         return this.props.item.content.map(e => {
@@ -100,7 +100,7 @@ class Post extends React.Component{
                     <Icon icon={"save"} round={true} padding={'3px'}/>
                     {/* <!-- Place this tag where you want the share button to render. --> */}
                     <div id="gplus" className={'gplus'} data-action="share">shar</div>
-                    <a href="https://plus.google.com/share?url={http://illulli-1e5a.com/posts/hello/}" onclick="javascript:window.open(this.href,
+                    <a href="https://plus.google.com/share?url=http://illulli-1e5a.com/posts/hello/" onclick="javascript:window.open(this.href,
   '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;"><img
   src="https://www.gstatic.com/images/icons/gplus-64.png" alt="Share on Google+"/></a>
                 </Shareable>
